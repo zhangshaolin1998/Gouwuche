f29ee177480257f246a69bf74221512edb965b85
zhangshaolin <1663708720@qq.com>
2017-11-01 18:25:02 +0800

    Initial commit

---
+++ Gouwuche/src/main/java/bean/CartBean.java
@@ -0,0 +1,15 @@
+package bean;
+
+/**
+ * Created by MrHtison on 2017/9/9.
+ */
+public class CartBean {
+
+    /** 商品ID */
+    private String goodsid;
+    /** 店铺ID */
+    private String storeid;
+
+
+
+}
